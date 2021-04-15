9543fa563dc98f85683d9fcb62daeaaa19150f21
xinchengforgit <2419743144@qq.com>
2021-04-15 17:27:29 +0800

    学习了一下kruskal算法

--- disjoint_sets/MST.cpp
+++ disjoint_sets/MST.cpp
@@ -1,3 +1,56 @@
 #include <cstdio>
 #include <iostream>
+#include <algorithm>
 using namespace std;
+#define MAXN 100000
+int f[MAXN];
+int n, m; //n表示节点个数
+int tot;
+struct edge
+{
+    int from;
+    int to;
+    int w;
+} e[MAXN];
+
+void init()
+{
+    for (int i = 1; i <= n; i++)
+        f[i] = i;
+}
+int find(int x)
+{
+    if (f[x] == x)
+        return;
+    else
+        return x = find(f[x]);
+}
+void merge(int a, int b)
+{
+    int root1 = find(a);
+    int root2 = find(b);
+    if (root1 != root2)
+    {
+        f[root1] = root2;
+    }
+}
+bool cmp(edge a, edge b)
+{
+    return a.w < b.w;
+}
+void kruskal()
+{
+    sort(e + 1, e + 1 + m, cmp);
+    for (int i = 1; i <= m; i++)
+    {
+        if (find(e[i].from) != find(e[i].to))
+        {
+            merge(e[i].from, e[i].to); //没有在一起，则合并
+            tot += e[i].w;
+        }
+    }
+}
+int main()
+{
+    cin >> n >> m;
+}
--- disjoint_sets/disjoint_set.cpp
+++ disjoint_sets/disjoint_set.cpp
@@ -1,19 +1,18 @@
 #include <cstdio>
 #include <iostream>
+#include <algorithm>
 using namespace std;
+#define MAXN 100000
 //普通版并查集,用一个数组来储存当前节点的父节点
-int f[10000];
+int f[MAXN];
 //初始时，所有的节点的父节点都是自己
-int n, m; //n表示节点个数
+int n;
 void init()
 {
     for (int i = 1; i <= n; i++)
         f[i] = i;
 } //初始化集合，每个集合一开始都只有一个节点
-void merge(int a, int b)
-{
-    f[a] = b;
-}
+
 int find(int a)
 {
     if (f[a] == a)
@@ -36,12 +35,21 @@ int find1(int a)
     }
 }
 //考虑路径压缩
-int find3(int a)
+int find2(int a)
 {
     if (a == f[a])
         return a;
     else
     {
-        return a = find(f[a]); //每次向上的过程中可以同时更新每一个经过的父节点，从而将路径压缩
+        return a = find2(f[a]); //每次向上的过程中可以同时更新每一个经过的父节点，从而将路径压缩
     }
 }
+void merge(int a, int b)
+{
+    int root1 = find2(a);
+    int root2 = find2(b);
+    if (root1 != root2)
+    {
+        f[root1] = root2;
+    }
+} //合并
